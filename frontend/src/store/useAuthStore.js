3fc92abdc7ad17d81d3138b8fab9e57188072276
veeraj2404 <virajwasnik24@gmail.com>
2025-01-06 21:46:19 +0530

    profile section is completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -56,5 +56,18 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isLoggingIn: false })
         }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data })
+            toast.success("Profile updated successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUpdatingProfile: false })
+        }
     }
 }))

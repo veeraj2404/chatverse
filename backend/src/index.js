f84038f33a694130bf06d04a9eb60232c514bcc3
veeraj2404 <virajwasnik24@gmail.com>
2025-01-08 18:04:40 +0530

    socket inplemented to find online users

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+app
 
 const PORT = process.env.PORT;
 
@@ -21,7 +22,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("server is running on PORT: " + PORT);
     connectDB();
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,10 @@ import { useThemeStore } from './store/useThemeStore';
 
 const App = () => {
 
-    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+    const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
     const { theme } = useThemeStore();
+
+    console.log({ onlineUsers })
 
     useEffect(() => {
         checkAuth();

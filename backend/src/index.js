871172c91d889b535601a0bfbadf584f4be26500
veeraj2404 <virajwasnik24@gmail.com>
2025-01-09 18:26:41 +0530

    path fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '50mb' })); // Set request size limit here
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     credentials: true
 }));
 
